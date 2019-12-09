8385b49fc892bb958df4c8911629beda8c6f286a
jstoreinthehills <salimjjuma@gmail.com>
2019-12-09 12:22:43 +0300

    Added Question Factory and added User Factory using Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Model\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+       factory(User::class, 3)->create()->each(function ($user){
+            $user->questions()
+                ->saveMany(
+                    factory(App\Model\Question::class, rand(1,5))->make()
+                );
+       });
     }
 }
